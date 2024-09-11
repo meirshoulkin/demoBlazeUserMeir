d4ea2904c9b8e1dbbc8b9b201cb2d1dfd1870689
meirshoulkin <meirshoulkin@gmail.com>
2024-09-11 23:17:56 +0300

    as55

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,24 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+
+    protected static WebDriver driver;
+
+    public void setDriver(WebDriver wd) {
+
+        driver = wd;
+    }
+
+    public void waitElement(WebElement element, int seconds) {
+        new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.visibilityOf(element));
+    }
 }
+
